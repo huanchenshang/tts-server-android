a7fca46739ce783bb3270410c32b1f533a93457b
jing <42014615+jing332@users.noreply.github.com.>
2025-02-25 16:52:01 +0800

    refactor: UI changes for QuickEditBottomSheet and BgmConfigUI
    - Adjusted padding for items in `QuickEditBottomSheet`.
    - Added vertical padding to `LabelSlider` in `BgmConfigUI`.
    - Add `Modifier.padding(8.dp)` to `Column` in `BgmConfigUI`.

--- lib-compose/src/main/java/com/github/jing332/compose/widgets/InitSystemNavigation.kt
+++ lib-compose/src/main/java/com/github/jing332/compose/widgets/InitSystemNavigation.kt
@@ -2,7 +2,7 @@ package com.github.jing332.compose.widgets
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -10,7 +10,7 @@ import com.google.accompanist.systemuicontroller.rememberSystemUiController
 fun InitSystemNavigation(color: Color = MaterialTheme.colorScheme.surface) {
     val uiController = rememberSystemUiController()
 
-    LaunchedEffect(color) {
+    SideEffect {
         uiController.setNavigationBarColor(color)
     }
 }
