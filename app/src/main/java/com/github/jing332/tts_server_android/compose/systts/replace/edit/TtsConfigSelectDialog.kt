cfab41b8e0d4bbaa2dce0f0dc2499201bbb58773
jing <42014615+jing332@users.noreply.github.com.>
2025-03-01 20:08:50 +0800

    feat: Fix scrolling in bottom sheet and update select dialog
    - Fixed issue where scrolling a LazyColumn inside a BottomSheet would cause the BottomSheet itself to scroll instead.
    - Modified `SysttsSelectBottomSheet` to display a list of enabled TTS configurations using `LazyColumn`.
    - Added `HtmlText` to display the description of the TTS configurations.
    - Added padding to the LazyColumn items for better readability.
    - Fixed issue where `SysttsSelectBottomSheet` in `ReplaceRuleEditScreen` was commented out.

--- app/src/main/java/com/github/jing332/tts_server_android/compose/systts/replace/edit/TtsConfigSelectDialog.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/compose/systts/replace/edit/TtsConfigSelectDialog.kt
@@ -1,10 +1,8 @@
 package com.github.jing332.tts_server_android.compose.systts.replace.edit
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -20,13 +18,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.jing332.compose.ComposeExtensions.clickableRipple
+import com.github.jing332.compose.widgets.HtmlText
 import com.github.jing332.database.dbm
 import com.github.jing332.database.entities.systts.SystemTtsV2
+import com.github.jing332.database.entities.systts.TtsConfigurationDTO
 import com.github.jing332.tts_server_android.R
+import com.github.jing332.tts_server_android.compose.systts.list.ui.ItemDescriptorFactory
 import com.github.jing332.tts_server_android.compose.theme.AppTheme
 
 @Preview
@@ -42,7 +44,8 @@ private fun PreviewTtsConfigSelectDialog() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun SysttsSelectBottomSheet(onDismissRequest: () -> Unit, onClick: (SystemTtsV2) -> Unit) {
-    val items = remember { dbm.systemTtsV2.allEnabled }
+    val items = remember { dbm.systemTtsV2.allEnabled.filter { it.config is TtsConfigurationDTO } }
+    val context = LocalContext.current
     ModalBottomSheet(onDismissRequest = onDismissRequest) {
         Column(
             Modifier
@@ -54,9 +57,12 @@ internal fun SysttsSelectBottomSheet(onDismissRequest: () -> Unit, onClick: (Sys
                 text = stringResource(id = R.string.choice_item, ""),
                 style = MaterialTheme.typography.titleLarge
             )
-            Spacer(modifier = Modifier.height(8.dp))
-            LazyColumn(Modifier.weight(1f)) {
+            LazyColumn(Modifier
+                .weight(1f)
+                .padding(8.dp)) {
                 itemsIndexed(items) { _, systts ->
+                    val descriptor =
+                        remember(systts) { ItemDescriptorFactory.from(context, systts) }
                     Column(
                         Modifier
                             .fillMaxWidth()
@@ -64,15 +70,16 @@ internal fun SysttsSelectBottomSheet(onDismissRequest: () -> Unit, onClick: (Sys
                             .clickableRipple {
                                 onClick(systts)
                             }
+                            .padding(4.dp)
                     ) {
                         Text(
-                            text = systts.displayName ?: "",
+                            text = systts.displayName,
                             style = MaterialTheme.typography.titleMedium
                         )
-//                        HtmlText(
-//                            text = systts.tts.getDescription(),
-//                            style = MaterialTheme.typography.bodySmall
-//                        )
+                        HtmlText(
+                            text = descriptor.desc,
+                            style = MaterialTheme.typography.bodySmall
+                        )
                     }
                 }
             }
